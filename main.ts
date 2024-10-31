0478fbe990cd30bee4b4925e3be240a66a2d26bc
skuru1244 <skurua1244@gmail.com>
2024-11-01 08:28:41 +0900

    단어 추가 으럇 걸쭉

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ const textarea = <HTMLTextAreaElement>document.getElementById("nong");
 const count = <HTMLSpanElement>document.getElementById("count");
 
 const words: Array<string> =
-  "말랑 질퍽 끈적 화끈 푹신 보들 매끈 쭈욱 뷰릇 찰팍 주물 찌릿 질척 두근 덜렁 찰싹 쿵짝 빨딱 푸슉 움찔 따끈 모락 욱신 축축 쫄깃 꼴깍 첨벙 대롱 주렁 푹푹 꼬물 오물 바들 포근 미끌 콸콸 찔끔 꿀꺽 찰방 킁킁 듬뿍 꿈틀 가득 덥석 꼿꼿 바싹 빙글 벌렁 불쑥 벌컥 쥬릇 헐떡 줄줄 흘깃 후끈 출렁 퓨웃 할짝 꿀럭 츄웁 찔꺽 꾸욱 오싹 머뭇 화악 큐웅 휘청".split(
+  "말랑 질퍽 끈적 화끈 푹신 보들 매끈 쭈욱 뷰릇 찰팍 주물 찌릿 질척 두근 덜렁 찰싹 쿵짝 빨딱 푸슉 움찔 따끈 모락 욱신 축축 쫄깃 꼴깍 첨벙 대롱 주렁 푹푹 꼬물 오물 바들 포근 미끌 콸콸 찔끔 꿀꺽 찰방 킁킁 듬뿍 꿈틀 가득 덥석 꼿꼿 바싹 빙글 벌렁 불쑥 벌컥 쥬릇 헐떡 줄줄 흘깃 후끈 출렁 퓨웃 할짝 꿀럭 츄웁 찔꺽 꾸욱 오싹 머뭇 화악 큐웅 휘청 으럇 걸쭉".split(
     " "
   );
 count.innerText = words.length.toString();
