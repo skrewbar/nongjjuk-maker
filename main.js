de1a4dbf50a18b3c3bdc6de4a871491e0a250794
skuru1244 <skurua1244@gmail.com>
2024-11-01 10:23:39 +0900

    단어 100개까지 추가

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
+var words = "말랑 질퍽 끈적 화끈 푹신 보들 매끈 쭈욱 뷰릇 찰팍 주물 찌릿 질척 두근 덜렁 찰싹 쿵짝 빨딱 푸슉 움찔 따끈 모락 욱신 축축 쫄깃 꼴깍 첨벙 대롱 주렁 푹푹 꼬물 오물 바들 포근 미끌 콸콸 찔끔 꿀꺽 찰방 킁킁 듬뿍 꿈틀 가득 덥석 꼿꼿 바싹 빙글 벌렁 불쑥 벌컥 쥬릇 헐떡 줄줄 흘깃 후끈 출렁 퓨웃 할짝 꿀럭 츄웁 찔꺽 꾸욱 오싹 머뭇 화악 큐웅 휘청 으럇 걸쭉 쫀득 철퍽 찌익 쭈룩 탁탁 딱딱 쩌억 쪼옥 벌떡 쏘옥 쭈걱 그득 불끈 물컹 질겅 꾸덕 질펀 쭈왑 뿍짝 뻐끔 팔딱 빳빳 씰룩 흠칫 꿀렁 아슬 헐렁 반들 처덕 날름 꿀떡".split(" ");
 var front = "와캬퍄헉농쭉튼빵뿅";
 var back = "위풍당당민달팽이섹스";
 var textarea = document.getElementById("nong");
 var count = document.getElementById("count");
-var words = "말랑 질퍽 끈적 화끈 푹신 보들 매끈 쭈욱 뷰릇 찰팍 주물 찌릿 질척 두근 덜렁 찰싹 쿵짝 빨딱 푸슉 움찔 따끈 모락 욱신 축축 쫄깃 꼴깍 첨벙 대롱 주렁 푹푹 꼬물 오물 바들 포근 미끌 콸콸 찔끔 꿀꺽 찰방 킁킁 듬뿍 꿈틀 가득 덥석 꼿꼿 바싹 빙글 벌렁 불쑥 벌컥 쥬릇 헐떡 줄줄 흘깃 후끈 출렁 퓨웃 할짝 꿀럭 츄웁 찔꺽 꾸욱 오싹 머뭇 화악 큐웅 휘청 으럇 걸쭉".split(" ");
 count.innerText = words.length.toString();
 function shuffle(array) {
     var _a;
@@ -16,8 +16,8 @@ function shuffle(array) {
         ], array[currentIndex] = _a[0], array[randomIndex] = _a[1];
     }
 }
-function generateNongjjuk() {
+function refreshTextarea() {
     shuffle(words);
-    return front + words.join("") + back;
+    textarea.value = front + words.join("") + back;
 }
-textarea.value = front + words.join("") + back;
+refreshTextarea();
